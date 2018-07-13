281ee0839ea0e5715fe1865112a2b0b81a059c9a
Josh Granek <josh@duke.edu>
2018-07-13 07:36:01 +0000

    Made genome_prep.ipynb and mapping.ipynb from 2017 mapping.ipynb. Updated to used 2018 data and STAR instead of Tophat

--- bioinformatics/bioinf_intro_config.sh
+++ bioinformatics/bioinf_intro_config.sh
@@ -8,3 +8,7 @@ export RAW_FASTQS="$DATA_BASE/Granek_4837_180427A5"
 export CUROUT=$HOME/work/scratch/bioinf_intro
 export TRIMMED=$CUROUT/trimmed_fastqs
 export MYINFO=$CUROUT/myinfo
+export GENOME_DIR=$CUROUT/genome
+export STAR_OUT=$CUROUT/star_out
+export FASTA=${GENOME_DIR}/Cryptococcus_neoformans_var_grubii_h99.CNA3.dna.toplevel.fa
+export GTF=${GENOME_DIR}/Cryptococcus_neoformans_var_grubii_h99.CNA3.39.gtf
