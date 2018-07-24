e3c067065cf6be0f4b389b8b6698d97669e34501
Josh Granek <>
2018-07-24 16:14:34 -0400

    prep_local_jupyter.sh: run docker directly

--- reproducibility/prep_local_jupyter.sh
+++ reproducibility/prep_local_jupyter.sh
@@ -7,7 +7,7 @@ JUPYTER_DIR=$TARGET_DIR/jupyter-HTS-2018
 NOTEBOOK_DIR=$WORK_DIR/HTS2018-notebooks
 
 
-
+rm -rf $NOTEBOOK_DIR $JUPYTER_DIR
 mkdir -p $WORK_DIR $DATA_DIR
 JUPYTER_PASWORD="dklf8FHidsah98gdpoadjsf"
 
@@ -24,15 +24,14 @@ DownloadData() {
 BuildAndRunImage() {
     git clone git@gitlab.oit.duke.edu:HTS2018/jupyter-HTS-2018.git $JUPYTER_DIR
     docker build -t mccahill/jupyter-hts-2018 $JUPYTER_DIR
-    echo "docker run --name jupyter-hts-2018 \
+    docker run --name jupyter-hts-2018 \
       -e USE_HTTPS=yes \
       -d -p 9999:8888 \
       -e PASSWORD="$JUPYTER_PASWORD" \
       -v ${DATA_DIR}:/data \
       -v ${WORK_DIR}:/home/jovyan/work \
       -e NB_UID=1000 \
-      mccahill/jupyter-hts-2018" > $TARGET_DIR/run
-    bash $TARGET_DIR/run
+      mccahill/jupyter-hts-2018
 }
 
 BuildAndRunImage
@@ -41,5 +40,5 @@ DownloadData
 # # Clone Notebook Repo
 git clone git@gitlab.oit.duke.edu:HTS2018/HTS2018-notebooks.git $NOTEBOOK_DIR
 
-echo "JUPYTER URL is <https://localhost:9999>"
-echo "JUPYTER_PASWORD: $JUPYTER_PASWORD"
+printf "\n\nJUPYTER URL is <https://localhost:9999>\n"
+printf "JUPYTER_PASWORD: $JUPYTER_PASWORD\n\n"
