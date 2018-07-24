b3d6d3c30fd0edb602e955e208af327e8765e3bb
Josh Granek <>
2018-07-24 13:36:46 -0400

    prep_local_jupyter.sh: revising

---
+++ reproducibility/prep_local_jupyter.sh
@@ -0,0 +1,45 @@
+TARGET_DIR=${$1:-hts2018}
+
+WORK_DIR=$TARGET_DIR/jovyan/work
+DATA_DIR=$TARGET_DIR/data/hts2018_pilot
+mkdir -p $WORK_DIR $DATA_DIR
+JUPYTER_PASWORD="dklf8FHidsah98gdpoadjsf"
+
+
+# Clone Repos
+cd $WORK_DIR
+git clone git@gitlab.oit.duke.edu:HTS2018/HTS2018-notebooks.git
+git clone git@gitlab.oit.duke.edu:Biostatistics_and_Bioinformatics/HTS_SummerCourse_2017.git
+
+# Get data subset
+
+
+rsync vcm3:/tmp/hts2018/Granek_4837_180427A5/27_MA_P_S38_L002_R1_001.fastq.gz $DATA_DIR
+rsync  vcm3:/tmp/hts2018/Granek_4837_180427A5/Granek_4837_180427A5.checksum $DATA_DIR
+
+rsync -v --progress --stats  vcm3:/tmp/hts2018/Granek_4837_180427A5/27_MA_P_S38_L00?_R1_001.fastq.gz $DATA_DIR
+rsync -v --progress --stats  vcm3:/tmp/hts2018/Granek_4837_180427A5/3_MA_*.fastq.gz $DATA_DIR
+rsync -v --progress --stats  vcm3:/tmp/hts2018/Granek_4837_180427A5/35_MA_*.fastq.gz $DATA_DIR
+rsync -v --progress --stats  vcm3:/tmp/hts2018/Granek_4837_180427A5/24_MA_*.fastq.gz $DATA_DIR
+
+# md5sum -vc Granek_4837_180427A5/Granek_4837_180427A5.checksum 
+# md5sum -vc Granek_4837_180427A5/Granek_4837_180427A5.checksum | grep -v open
+
+# Build Jupyter Docker Image
+BuildAndRunImage() {
+    git clone git@gitlab.oit.duke.edu:HTS2018/jupyter-HTS-2018.git $TARGET_DIR
+    docker build -t mccahill/jupyter-hts-2018 $TARGET_DIR/jupyter-HTS-2018
+    echo "docker run --name jupyter-hts-2018 \
+      -e USE_HTTPS=yes \
+      -d -p 9999:8888 \
+      -e PASSWORD="$JUPYTER_PASWORD" \
+      -v ${DATA_DIR}:/data \
+      -v ${WORK_DIR}:/home/jovyan/work \
+      -e NB_UID=1000 \
+      mccahill/jupyter-hts-2018" > $TARGET_DIR/run
+    bash $TARGET_DIR/run
+}
+
+BuildAndRunImage &
+
+echo "JUPYTER_PASWORD: $JUPYTER_PASWORD"
