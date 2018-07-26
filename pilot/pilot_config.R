2440f88a3c90e6a58c129d6e957abefef4dba1dc
clint <clint00724@gmail.com>
2018-07-26 01:35:31 +0000

    expand home directory from '~' to /home/jovyan; to fix tool::md5sum in R

--- pilot/pilot_config.R
+++ pilot/pilot_config.R
@@ -9,11 +9,11 @@ library(dendextend)
 
 # set directories
 DATDIR <- "/data/hts2018_pilot/star_counts"
-CURDIR <- "~/work/scratch/analysis_output"
+CURDIR <- "/home/jovyan/work/scratch/analysis_output"
 OUTDIR <- file.path(CURDIR, "out")
 IMGDIR <- file.path(CURDIR, "img")
 
 # Metadata (metadtfile)
-METADTFILE <- "~/work/HTS2018-notebooks/josh/info/2018_pilot_metadata_anon.tsv"
+METADTFILE <- "/home/jovyan/work/HTS2018-notebooks/josh/info/2018_pilot_metadata_anon.tsv"
 
 
