d987d289aa8708ba93f4dcb95903b8c93e692871
Josh Granek <josh@duke.edu>
2018-07-27 21:09:02 +0000

    Added scripts for downloading and analyzing course data

---
+++ bioinformatics/prep_nb/star_2018_course_config.sh
@@ -0,0 +1,26 @@
+set -u
+
+# Run Configs
+export THREADS=2
+export MAX_JOBS=8
+
+# Input
+export DATA_BASE="/data/hts2018_course"
+export RAW_FASTQS="$DATA_BASE/Granek_5003_180726A5"
+export INFO="../info"
+export ADAPTERS="$INFO/neb_E7600_adapters_withrc.fasta"
+
+# Output
+export CUROUT=$HOME/work/scratch/hts2018_course
+export TRIMMED=$CUROUT/trimmed_fastqs
+export GENOME_DIR=$CUROUT/genome
+export STAR_OUT=$CUROUT/star_out
+export FINAL_COUNTS=$DATA_BASE/star_counts
+
+# Genome
+export FA_URL="ftp://ftp.ensemblgenomes.org/pub/release-39/fungi/fasta/fungi_basidiomycota1_collection/cryptococcus_neoformans_var_grubii_h99/dna/Cryptococcus_neoformans_var_grubii_h99.CNA3.dna.toplevel.fa.gz"
+export GTF_URL="ftp://ftp.ensemblgenomes.org/pub/release-39/fungi/gtf/fungi_basidiomycota1_collection/cryptococcus_neoformans_var_grubii_h99/Cryptococcus_neoformans_var_grubii_h99.CNA3.39.gtf.gz"
+export GFF_URL="ftp://ftp.ensemblgenomes.org/pub/release-39/fungi/gff3/fungi_basidiomycota1_collection/cryptococcus_neoformans_var_grubii_h99/Cryptococcus_neoformans_var_grubii_h99.CNA3.39.gff3.gz"
+export GFF=$(basename ${GFF_URL%.gz})
+export GTF=$(basename ${GTF_URL%.gz})
+export FA=$(basename ${FA_URL%.gz})
