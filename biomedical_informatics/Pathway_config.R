cf13dc4960e4565eed7535aa05c2fb72ae7c3333
clint <clint00724@gmail.com>
2018-07-30 14:34:54 +0000

    change the directory of info folder in configuration files

--- biomedical_informatics/Pathway_config.R
+++ biomedical_informatics/Pathway_config.R
@@ -8,4 +8,4 @@ library(pheatmap)
 CURDIR <- "/home/jovyan/work/scratch/analysis_output"
 OUTDIR <- file.path(CURDIR, "out")
 IMGDIR <- file.path(CURDIR, "img")
-INFODIR <- "/home/jovyan/work/HTS2018-notebooks-repo/HTS2018-notebooks/biomedical_informatics/Info"
+INFODIR <- "info"
--- biomedical_informatics/Pathway_config.sh
+++ biomedical_informatics/Pathway_config.sh
@@ -9,8 +9,7 @@ set -u
 CURDIR="/home/jovyan/work/scratch/analysis_output"
 OUTDIR="${CURDIR}/out"
 IMGDIR="${CURDIR}/img"
-INFODIR="/home/jovyan/work/HTS2018-notebooks/biomedical_informatics"
-INFODIR="/home/jovyan/work/HTS2018-notebooks-repo/HTS2018-notebooks/biomedical_informatics/Info"
+INFODIR="info"
 PATHFILE="${INFODIR}/PathwaysByGeneIds_Summary.txt"
 
 # python script
