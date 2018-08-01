2829d58f0893519afa943223c70be325f35af4d1
Josh Granek <josh@duke.edu>
2018-08-01 07:27:11 +0000

    star_2018_course.ipynb: added BAM output

--- bioinformatics/prep_nb/star_2018_course_config.sh
+++ bioinformatics/prep_nb/star_2018_course_config.sh
@@ -1,8 +1,8 @@
 set -u
 
 # Run Configs
-export THREADS=2
-export MAX_JOBS=8
+export THREADS=1
+export MAX_JOBS=6
 
 # Input
 export DATA_BASE="/data/hts2018_course"
@@ -16,6 +16,7 @@ export TRIMMED=$CUROUT/trimmed_fastqs
 export GENOME_DIR=$CUROUT/genome
 export STAR_OUT=$CUROUT/star_out
 export FINAL_COUNTS=$DATA_BASE/star_counts
+export FINAL_BAMS=$DATA_BASE/star_bams
 
 # Genome
 export FA_URL="ftp://ftp.ensemblgenomes.org/pub/release-39/fungi/fasta/fungi_basidiomycota1_collection/cryptococcus_neoformans_var_grubii_h99/dna/Cryptococcus_neoformans_var_grubii_h99.CNA3.dna.toplevel.fa.gz"
